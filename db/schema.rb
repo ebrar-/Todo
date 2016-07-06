ef25ec4fca734f45877ee6aba85e4b313b017009
mehmetadil <mehmetadil_323@hotmail.com>
2016-07-06 19:50:43 +0300

    user-item ilişkisi kuruldu

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160703195827) do
+ActiveRecord::Schema.define(version: 20160706164905) do
 
   create_table "items", force: :cascade do |t|
     t.text     "content",    limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "user_id",    limit: 4
   end
 
   create_table "users", force: :cascade do |t|
